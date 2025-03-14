ffb210555d500933afb0db0079c105081afdb51f
Juan Meta Sirgianto <juansirgianto.js@gmail.com>
2025-03-14 16:24:54 +0800

    abc

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       >
         <SidebarProvider>
           <AppSidebar />
-          <main>
+          <main className="flex flex-1 flex-col">
             <SidebarTrigger />
             {children}
           </main>
