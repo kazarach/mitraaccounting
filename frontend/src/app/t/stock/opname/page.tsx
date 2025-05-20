2ab22d91fa01ac515d3f16dff3c9cc1aebb3acb7
Juan Meta Sirgianto <juansirgianto.js@gmail.com>
2025-05-20 18:44:40 +0800

    opname v1

--- frontend/src/app/t/stock/opname/page.tsx
+++ frontend/src/app/t/stock/opname/page.tsx
@@ -1,190 +1,24 @@
-"use client";
-import React, { useEffect, useState } from 'react';
-
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow
-} from "@/components/ui/table";
 import {
   Card,
   CardContent,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Button } from '@/components/ui/button';
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
-import { cn } from '@/lib/utils';
-import { CalendarIcon, Check, ChevronsUpDown, Search, Trash } from 'lucide-react';
-import { Calendar } from "@/components/ui/calendar"
-import { format } from 'date-fns';
-import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '@/components/ui/command';
-import { useDispatch, useSelector } from 'react-redux';
-import { setTableData, deleteRow, clearTable } from '@/store/features/tableSlicer';
-import { RootState } from '@/store/store';
-import { toast } from 'sonner';
-import OrderSelling from '../../selling/order/page2';
-import TambahProdukModal from '@/components/modal/tambahProduk-modal';
-import { Dialog, DialogTrigger, DialogContent } from '@/components/ui/dialog';
+import OpnameTable from "./opnametabledata";
 
-const StockOpname = () => {
-
-  const distributors = [
-    {
-      value: "1",
-      label: "Distributor A",
-    },
-    {
-      value: "2",
-      label: "Distributor B",
-    },
-    {
-      value: "3",
-      label: "Distributor C",
-    },
-    {
-      value: "4",
-      label: "Distributor D",
-    },
-    {
-      value: "5",
-      label: "Distributor E",
-    },
-
-  ]
-
-  const [date, setDate] = React.useState<Date>()
-  const [open, setOpen] = React.useState(false)
-  const [value, setValue] = React.useState("")
-
-  const dispatch = useDispatch();
-  const data = useSelector((state: RootState) => state.table["opname"] || []);
-
-  useEffect(() => {
-    if (data.length === 0) {
-      dispatch(
-        setTableData({
-          tableName: "opname",
-          data: [],
-        })
-      );
-    }
-  }, [dispatch]);
-
-  const handleDelete = (id: number) => {
-    dispatch(deleteRow({ tableName: "opname", id }));
-    toast.error("Produk berhasil dihapus!");
-  };
-
-  const handleClear = () => {
-    dispatch(clearTable({ tableName: "opname" }));
-    toast.error("Table berhasil dihapus!");
-  };
-
+const OpnameStock = () => {
   return (
-    <div className="flex justify-left w-full pt-4">
+    <div className="flex justify-center w-full pt-4">
       <Card className="w-full mx-4">
         <CardHeader>
-          <CardTitle>Opname Persediaan</CardTitle>
+          <CardTitle>Opname Stock</CardTitle>
         </CardHeader>
         <CardContent>
-          <div className="flex flex-col space-y-4">
-            <div className="flex justify-between gap-4 mb-4">
-              <div className="flex flex-wrap items-end gap-4">
-                <div className="flex flex-col space-y-2">
-                  <Label htmlFor="date">Tanggal</Label>
-                  <Popover>
-                    <PopoverTrigger asChild>
-                      <Button
-                        variant={"outline"}
-                        className={cn(
-                          "w-[200px] justify-start text-left font-normal",
-                          
-                        )}
-                      >
-                        <CalendarIcon />
-                        {date ? format(date, "PPP") : <span>Pilih Tanggal</span>}
-                      </Button>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-auto p-0">
-                      <Calendar
-                        mode="single"
-                        selected={date}
-                        onSelect={setDate}
-                        initialFocus
-                      />
-                    </PopoverContent>
-                  </Popover>
-                </div>                
-              </div>
-              <div className='flex items-end gap-2'>
-                <Dialog>
-                  <DialogTrigger asChild>
-                    <Button className='font-medium bg-blue-500 hover:bg-blue-600'>Tambah Produk</Button>
-                  </DialogTrigger>
-                  <DialogContent className="w-[75vw] max-h-[90vh]">
-                    <TambahProdukModal tableName='opname'/>
-                  </DialogContent>
-                </Dialog>
-                <Button onClick={handleClear} className='border-red-500 border bg-white text-red-500 hover:bg-red-500 hover:text-white'>Batal</Button>
-              </div>
-            </div>
-
-            <div className="rounded-md border overflow-auto">
-              <Table>
-                <TableHeader>
-                  <TableRow>
-                    <TableHead>Barcode</TableHead>
-                    <TableHead className="text-left">Nama Barang</TableHead>
-                    <TableHead className="text-left">Satuan</TableHead>
-                    <TableHead className="text-left">Jumlah Sistem</TableHead>
-                    <TableHead className="text-left">Jumlah Fisik</TableHead>
-                    <TableHead className="text-left">Selisih</TableHead>
-                    <TableHead className="text-right">Action</TableHead>
-                </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {data.length === 0 ? (
-                    <TableRow>
-                      <TableCell colSpan={11} className="text-center text-gray-400 bg-gray-200">
-                        Belum menambahkan produk
-                      </TableCell>
-                    </TableRow>
-                  ) : (
-                  data.map((item) => (
-                    <TableRow key={item.id}>
-                        <TableCell className="font-medium">{item.jumlah_barang}</TableCell>
-                        <TableCell className="text-left">{item.produk}</TableCell>
-                        <TableCell className="text-left"><input type="number" className='text-left w-24 bg-gray-100 rounded-sm' placeholder='0' /></TableCell>
-                        <TableCell className="text-left">{item.isi_packing}</TableCell>
-                        <TableCell className="text-left">{item.isi_packing}</TableCell>
-                        <TableCell className="text-left">{item.id}</TableCell>
-                        <TableCell className="text-right">
-                            <Button onClick={() => handleDelete(item.id)} className='bg-red-500 hover:bg-red-600 size-7'>
-                              <Trash></Trash>
-                            </Button>
-                        </TableCell>
-                    </TableRow>
-                  ))
-                )}
-                </TableBody>
-              </Table>
-            </div>
-          <div className='flex gap-2 justify-end '>
-            <Button className='bg-blue-500 hover:bg-blue-600'>Opname</Button>
-            <Button className='bg-green-500 hover:bg-green-600'>Cetak</Button>
-          </div>
-          </div>
+          <OpnameTable tableName='opname' />
         </CardContent>
       </Card>
     </div>
   );
 };
 
-export default StockOpname; 
+export default OpnameStock; 
